2d8bf573877bab7e3940f7d0fef75b9bfa74c1ec
aahan-bagga <baggaaahan@gmail.com>
2023-06-21 13:03:16 -0700

    fix

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def main():
     m = int(input("males: "))
     f = int(input("females: "))
 
-    m_perc = m / (m+f)
-    f_perc = f / (m+f)
+    m_perc = m / (m+f) * 100
+    f_perc = f / (m+f) * 100
     print(f"Total students: {m+f}")
     print(f"Number males: {m}")
     print(f"Number females: {f}")
-    print(f'Percentage of males and females: {m_perc:.2%} {f_perc:.2%}')
+    print(f'Percentage of males and females: {m_perc}% {f_perc}%')
     """
 
     ##################################################
@@ -26,4 +26,4 @@ def main():
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
